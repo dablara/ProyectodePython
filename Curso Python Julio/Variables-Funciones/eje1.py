06c4b840ead64ffb24a183d7c5ffdea93f2167fb
dablara <daniblanco707@gmail.com>
2024-07-15 19:10:21 +0200

    15-07

---
+++ Curso Python Julio/Variables-Funciones/eje1.py
@@ -0,0 +1,49 @@
+# Variables
+
+mi_variable= "Mi variable de cadena"
+
+print(type(mi_variable))
+
+mi_variable_int = 10
+
+print (mi_variable_int)
+
+my_bool_variable = True
+
+print (type(my_bool_variable))
+
+
+# Concatenación 
+
+print(mi_variable, mi_variable_int, my_bool_variable)
+print("Este es el valor de: ",mi_variable)
+
+# Funciones
+ # Type
+print (type(mi_variable_int))
+# Len
+print(len(mi_variable))
+
+# Variable en una sola linea 
+
+name, surname, alias, age= "Daniel", "Blanco", "dblara", 28
+
+print("Mi nombre es",name, surname ," mi edad es ", age, " y mi alias es ", alias)
+
+
+#nombre = input("¿Cómo te llamas? ")
+#edad = int(input("¿Que edad tienes? "))
+
+#print(nombre)
+#print (edad)
+
+
+# Cambio de tipo
+
+nombre = 28
+edad = "Daniel"
+
+print (nombre)
+print (edad)
+
+
