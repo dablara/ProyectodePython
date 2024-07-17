1a82bbb7861db17c8adb3619327d0aab8823814c
dablara <daniblanco707@gmail.com>
2024-07-17 05:16:54 +0200

    17-07

---
+++ Proyectos/menu.py
@@ -0,0 +1,28 @@
+# Menú 
+
+print( " Bienvenido al menú de opciones")
+
+ops = True
+while ops:
+    print( """
+    1.Crear Usuario
+    2.Listar Usuarios 
+    3.Borrar Usuario
+    4.Salir 
+    """)
+          
+    ops= input("¿Que opcion deseas? ")
+    if ops == "1":
+       print("")
+    elif ops == "2":
+        print("")
+    elif ops == "3":
+        print ("")
+    elif ops == "4":
+        print ("\n Adios")
+        quit()
+       
+    elif ops !="":
+        print ("Elige una opción correcta")
+        
+        
