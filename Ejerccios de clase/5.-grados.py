1925433bc02a3b31246e90a1904f0fe3c359bca7
Daniel Blanco <34427276+dablara@users.noreply.github.com>
2024-09-16 18:48:03 +0200

    Delete Ejerccios de clase directory

--- Ejerccios de clase/5.-grados.py
+++
@@ -1,9 +0,0 @@
- # 5. Escribir un programa que convierta un valor dado en grados Fahrenheit a grados Celsius.
- # Autor: Daniel Blanco Aranda
-
-
-faren = int(input('¿Cuantos grados Fahrenheit hay? '))
-
-celcius = (faren - 32)* 5/9
-
-print (f'La conversion a grado celcius es {celcius}')
