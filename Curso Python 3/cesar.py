902d4d45bc9679ed768af3b2a19250e4cc1e40ef
dablara <danielblanco707@gmail.com>
2025-02-14 08:46:15 +0100

    actulización

---
+++ Curso Python 3/cesar.py
@@ -0,0 +1,5 @@
+# Crear un programa Python que permita cifrar y descifrar mediante el sistema César
+# El programa debe poder
+    # 1. Recibir un parámetro numérico.
+    # 2. Recibir un texto en claro o un texto cifrado.
+    # 3. Indicar la acción que queremos realizar: cifrar o descifrar.
