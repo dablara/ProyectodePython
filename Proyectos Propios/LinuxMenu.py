2b15f80690629654688e2eaa6771a525ce751d09
dablara <daniblanco707@gmail.com>
2025-02-26 13:18:59 +0100

    cambios2

--- Proyectos Propios/LinuxMenu.py
+++ Proyectos Propios/LinuxMenu.py
@@ -4,9 +4,9 @@
 # Fecha: 26-02-2025 
 
 
-# Importamos las librerías:
+# Importamos las librerías
 import subprocess # Nos permite ejecutar comando tanto en Linux como en Windows
-import os # Nos permite interactuar con el Sistema operativo..
+import os # Nos permite interactuar con el Sistema operativo.
 
 print("Bienvenido al menú de opciones")
 print ("En este menú podras elegir entre varias opciones para una buena Administración de Linux. Para cualquier duda contacta conmigo")
